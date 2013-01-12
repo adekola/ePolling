e0d3ed9c34b691fcc08b74a2f0797de395cc8abe
adekola <4martechnologies@gmail.com>
2013-01-12 02:03:51 +0100

    updates to views
    frames for the login, logout, register views

--- ePolling/urls.py
+++ ePolling/urls.py
@@ -5,5 +5,8 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'pollerApp.views.home', name='home')
+    url(r'^$', 'pollerApp.views.home', name='home'),
+    url(r'^login/$', 'pollerApp.views.login', name='login'),
+    url(r'^logout/$', 'pollerApp.views.logout', name='logout'),
+    url(r'^register/$', 'pollerApp.views.register', name='register')
     )
--- pollerApp/views.py
+++ pollerApp/views.py
@@ -1,6 +1,18 @@
 # Create your views here.
 
 from django.http import HttpResponse
+import datetime
 
 def home(request):
-    return HttpResponse("Welcome to ePolling system")
+    now  = datetime.date.now()
+    html = "<html><body> Welcome to ePolling system <br/> Today is '%s'.</body></html>"%now
+    return HttpResponse(html)
+
+def login(request):
+    return HttpResponse("login here")
+
+def logout(request):
+    return HttpResponse("Logout redirects here")
+
+def register(request):
+    return HttpResponse("Register here")
